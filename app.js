593d0b754092f9994c8617662d573e809340b0a3
MrSooo <51949526+MrSooo@users.noreply.github.com>
2021-11-03 15:54:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const classes = require("./routes/classRoute");
 
 const app = express();
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 const db = require("./models/database");
 db.sequelize.sync();
 
